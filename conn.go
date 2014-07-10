1ba75b28e82a96570844a0fc87a7b572a37ba00d
jackeychen <jackeychen@clustertech.com>
2014-07-10 15:04:30 +0800

    First draft

--- conn.go
+++ conn.go
@@ -4,13 +4,82 @@
 package socket
 
 import (
+  "encoding/json"
   "github.com/gorilla/websocket"
 )
 
 type Conn struct {
-  *websocket.Conn
+  c        *websocket.Conn
   sessions map[string]interface{}
-  id string
+  id       string
 
-  hub *Hub
+  hub  *Hub
+  send chan []byte
+}
+
+func (conn *Conn) ID() string {
+  return conn.id
+}
+
+func (conn *Conn) Release() {
+  conn.hub.Release(conn)
+}
+
+func (conn *Conn) readProcess() {
+  defer conn.Release()
+  for {
+    _, message, err := conn.c.ReadMessage()
+    if err != nil {
+      break
+    }
+    m := make(map[string]string)
+    err = json.Unmarshal(message, &m)
+    if err != nil {
+      break
+    }
+    e := &event{name: m["name"], msg: m["msg"], conn: conn}
+    fn, ok := conn.hub.handlers[e.name]
+    if ok {
+      fn(e)
+    }
+  }
+}
+
+func (conn *Conn) writeProcess() {
+  defer conn.Release()
+  for {
+    select {
+    case message, ok := <-conn.send:
+      if !ok {
+        conn.c.WriteMessage(websocket.CloseMessage, []byte{})
+        return
+      }
+      if err := conn.c.WriteMessage(websocket.TextMessage, message); err != nil {
+        return
+      }
+    }
+  }
+}
+
+func (conn *Conn) GetHub() *Hub {
+  return conn.hub
+}
+
+func (conn *Conn) Process() {
+  go conn.readProcess()
+  conn.writeProcess()
+}
+
+func (conn *Conn) Send(e string, msg string) {
+  m := make(map[string]string)
+  m["name"] = e
+  m["msg"] = msg
+  data, _ := json.Marshal(m)
+  conn.send <- data
+}
+
+func (conn *Conn) BroadCast(e string, msg string) {
+  for _, c := range conn.hub.conns {
+    c.Send(e, msg)
+  }
 }
--- hub.go
+++ hub.go
@@ -4,10 +4,10 @@
 package socket
 
 import (
+  "crypto/rand"
   "github.com/gorilla/websocket"
   "net/http"
-
-  "crypto/rand"
+  "sync"
 )
 
 const (
@@ -19,7 +19,7 @@ func randString(n int) string {
   var bytes = make([]byte, n)
   rand.Read(bytes)
   for i, b := range bytes {
-    bytes[i] = alphanum[b % byte(len(alphanum))]
+    bytes[i] = alphanum[b%byte(len(alphanum))]
   }
   return string(bytes)
 }
@@ -27,29 +27,53 @@ func randString(n int) string {
 type event struct {
   name string
   msg  string
-  conn    *Conn
+  conn *Conn
 }
 
 type Hub struct {
-  conns map[string]*Conn
-  ch chan *event
+  conns    map[string]*Conn
+  ch       chan *event
   handlers map[string]func(e *event)
   upgrader *websocket.Upgrader
+
+  locker *sync.RWMutex
 }
 
 func NewHub(upgrader *websocket.Upgrader) *Hub {
   conns := make(map[string]*Conn)
   ch := make(chan *event)
   handlers := make(map[string]func(e *event))
-  return &Hub{conns, ch, handlers, upgrader}
+  locker := new(sync.RWMutex)
+  return &Hub{conns, ch, handlers, upgrader, locker}
 }
 
-func (hub *Hub) Upgarde(w http.ResponseWriter, r *http.Request) (*Conn, error) {
+func (hub *Hub) On(e string, f func(e *event)) {
+  hub.handlers[e] = f
+}
+
+func (hub *Hub) Upgrade(w http.ResponseWriter, r *http.Request) (*Conn, error) {
   conn, err := hub.upgrader.Upgrade(w, r, nil)
   if err != nil {
     return nil, err
   }
   id := randString(idLength)
   sessions := make(map[string]interface{})
-  return &Conn{conn, sessions, id, hub}, nil
+  ch := make(chan []byte)
+
+  hub.locker.Lock()
+  defer hub.locker.Unlock()
+  hub.conns[id] = &Conn{conn, sessions, id, hub, ch}
+  return hub.conns[id], nil
+}
+
+func (hub *Hub) Release(conn *Conn) {
+  id := conn.ID()
+  delete(hub.conns, id)
+  close(conn.send)
+}
+
+func (hub *Hub) Get(id string) *Conn {
+  hub.locker.RLock()
+  defer hub.locker.RUnlock()
+  return hub.conns[id]
 }
